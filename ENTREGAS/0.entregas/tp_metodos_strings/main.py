f39cc03f59472bd460a921cb9b0d62288150cb1b
lprats <lprats@cecaitra.org.ar>
2023-10-09 16:33:45 -0300

    9/10/23 - commmit

---
+++ ENTREGAS/0.entregas/tp_metodos_strings/main.py
@@ -0,0 +1,20 @@
+# cd /Users/lprats/Desktop/cursos/utn/1er-cuatrimestre/programacion1/ENTREGAS/0.entregas/tp_metodos_strings
+# python main.py
+
+from biblioteca import *
+limpiar_consola()
+
+string = "Bajó una mano del cielo y acariciando su pelo Rulo y señal de la cruz La caricia de Jesús Hizo posible el milagro Convirtió la red en tierra Del balón hizo palomas Que aterrizaban su paz En la isla soledad Borrando una absurda guerra"
+
+
+string = "Universidad Tecnológica Nacional Facultad Regional Avellaneda"
+
+string = "administracion federal ingresoso publicos"
+
+print(obtener_acronimo(string))
+
+# print("".join(map(lambda palabra: palabra[0], string.split(" "))).upper())
+
+
+
+separador()
