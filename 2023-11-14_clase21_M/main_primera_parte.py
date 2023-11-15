2ed1461be391cdb309dd81357cdbb973bfdf147d
lionel-prats <lionel.prats.c@gmail.com>
2023-11-15 00:09:49 -0300

    martes 15/11/23 - commit 1

---
+++ 2023-11-14_clase21_M/main_primera_parte.py
@@ -0,0 +1,9 @@
+import pygame 
+
+# pygame.Surface.convert_alpha
+# https://www.pygame.org/docs/ref/surface.html#pygame.Surface.convert_alpha
+PLAYER = pygame.image.load("player.png").convert_alpha()
+
+
+
+
