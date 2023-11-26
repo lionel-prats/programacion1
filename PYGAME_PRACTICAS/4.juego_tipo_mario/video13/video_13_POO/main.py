e2d1ecc7d254d495e715076d153bc8cd77d3b2eb
lionel-prats <lionel.prats.c@gmail.com>
2023-11-26 16:26:22 -0300

    domingo 26/11 - commit 3

---
+++ PYGAME_PRACTICAS/4.juego_tipo_mario/video13/video_13_POO/main.py
@@ -0,0 +1,225 @@
+import pygame 
+from pygame.locals import * 
+from auxiliar import *
+from variables import *
+
+from modules.player import Player
+from modules.world import World
+from modules.button import Button
+from modules.font import Font
+from modules.coin import Coin
+
+limpiar_consola()
+
+pygame.init()
+
+configs = open_configs()
+
+clock = pygame.time.Clock()
+
+screen_dimentions = (configs.get("screen").get("screen_width"), configs.get("screen").get("screen_height"))
+screen = pygame.display.set_mode(screen_dimentions)
+pygame.display.set_caption("VIDEO 13 - POO")
+
+# define font
+# font = Font("Bauhaus 93", 70)
+# font_score = Font("Bauhaus 93", 30)
+# font_score_surface = font_score.surface_text
+
+# define game variables
+game_over = 0
+main_menu = True
+current_level = 1
+max_levels = 4
+score = 0 # monedas capturadas
+
+# load images 
+# sun_img = pygame.image.load(configs.get("screen").get("images").get("sky"))
+sun_img = pygame.image.load("img/sun.png")
+bg_img = pygame.image.load("img/sky.png")
+
+player = Player(configs.get("player1"))
+
+# blob, lava, etc
+enemies_group = pygame.sprite.Group()
+
+# platforms
+platform_group = pygame.sprite.Group()
+
+# exit
+utilities_group = pygame.sprite.Group()
+
+# coins
+coin_group = pygame.sprite.Group()
+
+
+world = World(configs.get("screen"), 
+              configs.get("enemies"), 
+              enemy_sprite_group=enemies_group,
+              utilities_configs=configs.get("utilities"),
+              utilities_group=utilities_group,
+              coin_group=coin_group,
+              platform_group=platform_group,
+              current_level=current_level)
+
+
+# create buttons 
+restart_button = Button(configs.get("buttons").get("restart"))
+start_button = Button(configs.get("buttons").get("start"))
+exit_button = Button(configs.get("buttons").get("exit"))
+
+coin_fx, jump_fx, game_over_fx = World.inicializar_sonidos()
+
+print(jump_fx)
+
+run = True
+while run:
+    # print(utilities_group)
+    # print(current_level)
+    
+    clock.tick(configs.get("fps"))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == \
+            pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            run = False
+
+    screen.blit(bg_img, (0,0))
+    screen.blit(sun_img, (100,100))
+    # world.draw_backgroung(screen)
+    
+    
+
+    if main_menu == True:
+        if start_button.draw(screen):
+            main_menu = False
+        if exit_button.draw(screen):
+            run = False
+    else:
+        world.draw(screen)
+
+        if game_over == 0:
+            enemies_group.update()
+            platform_group.update()
+            # si hay colision entre el player y alguna coin, elimino la coin e incremento score en 1 
+            #update score
+            #check if a coin has been collected
+            # True elimina de la pantalla el sprite colisionado
+            if pygame.sprite.spritecollide(player, coin_group, True): 
+                score += 1
+                coin_fx.play()
+            world.draw_text(screen, ("score", score, coin_group))
+            
+            # print(enemies_group)
+            # check for collision between player and enemies
+            if pygame.sprite.spritecollide(player, enemies_group, False):
+                game_over = -1
+                game_over_fx.play()
+                
+        # .draw -> metodo de la clase Group para blitear los elementos de un objeto de tipo Group (sprites)
+        coin_group.draw(screen)
+        enemies_group.draw(screen)
+        platform_group.draw(screen)
+        utilities_group.draw(screen) # puertas de salida
+
+        
+
+        # check for collision between player and exit
+        if pygame.sprite.spritecollide(player, utilities_group, False):
+            game_over = 1
+
+        
+        # if player has died
+        if game_over == -1:
+            world.draw_text(screen, ("game_over",))
+            if restart_button.draw(screen):
+                player.reset(configs.get("player1"))
+                game_over = 0
+                score = 0 
+                
+                enemies_group.empty()
+                utilities_group.empty()
+                platform_group.empty()
+
+                world = world.reset_level(
+                    configs.get("screen"), 
+                    configs.get("enemies"), 
+                    enemy_sprite_group=enemies_group,
+                    utilities_configs=configs.get("utilities"),
+                    utilities_group=utilities_group,
+                    coin_group=coin_group,
+                    platform_group=platform_group,
+                    current_level=current_level
+                )
+
+        # if player has completed the level
+        if game_over == 1: # el player llego a la puerta y paso de nivel
+            # reset game and go to the next level
+
+            current_level += 1
+            if current_level <= max_levels:
+                # reset level
+
+                enemies_group.empty()
+                utilities_group.empty()
+                platform_group.empty()
+
+                player.reset(configs.get("player1"))
+                # world_data = []
+                world = world.reset_level(
+                    configs.get("screen"), 
+                    configs.get("enemies"), 
+                    enemy_sprite_group=enemies_group,
+                    utilities_configs=configs.get("utilities"),
+                    utilities_group=utilities_group,
+                    coin_group=coin_group,                    
+                    platform_group=platform_group,
+                    current_level=current_level
+                ) # en la variable que guarda el objeto World, cargo una nueva instancia de World cada vez que el player supera un nivel
+                game_over = 0 # habilito que se siga moviendo el player y los enemigos
+            else: 
+                world.draw_text(screen, ("you_win",))
+                # restart game 
+                if restart_button.draw(screen):
+
+                    enemies_group.empty()
+                    utilities_group.empty()
+                    platform_group.empty()
+
+                    player.reset(configs.get("player1"))
+
+                    current_level = 1
+                    # world_data = []
+                    world = world.reset_level(
+                        configs.get("screen"), 
+                        configs.get("enemies"), 
+                        enemy_sprite_group=enemies_group,
+                        utilities_configs=configs.get("utilities"),
+                        utilities_group=utilities_group,
+                        coin_group=coin_group,
+                        platform_group=platform_group,
+                        current_level=current_level
+                    )
+                    game_over = 0 # habilito que se siga moviendo el player y los enemigos
+                    score = 0 
+
+        
+        # print(current_level)
+
+        player.update(
+                screen, 
+                configs.get("screen").get("screen_height"), 
+                tile_list = world.tile_list, 
+                game_over=game_over,
+                jump_fx=jump_fx
+            )
+
+
+        world.draw_grid(screen)
+
+    pygame.display.update()
+    
+pygame.quit()
+
+# cd /Users/User/Desktop/utn/cuatrimestre1/programacion_1/PYGAME_PRACTICAS/4.juego_tipo_mario/video13/video_13_POO
+# python main.py
---
+++ PYGAME_PRACTICAS/4.juego_tipo_mario/video13/video_13_POO/modules/world.py
@@ -0,0 +1,222 @@
+import pygame 
+# from modules.enemies.enemy import Enemy
+from modules.enemies.blob import Blob
+from modules.enemies.lava import Lava
+from modules.exit import Exit
+from modules.coin import Coin
+from modules.font import Font
+from modules.platform import Platform
+
+class World():
+
+    # def __init__(self, data, tile_size):
+    def __init__(
+            self, 
+            screen_configs, 
+            enemy_configs, 
+            enemy_sprite_group, 
+            utilities_configs, 
+            utilities_group, 
+            coin_group, 
+            platform_group, 
+            current_level):
+        
+        self.screen_configs = screen_configs
+        self.tile_list = []
+
+        #load images 
+        dirt_img = pygame.image.load(self.screen_configs.get("images").get("dirt")) 
+        grass_img = pygame.image.load(self.screen_configs.get("images").get("grass")) 
+        
+        # agrego al coin_group una coin para que se blitee en el score, arriba a la izquierda en la pantalla
+        score_coin = Coin(25, 28, 45)
+        coin_group.add(score_coin) 
+        
+        row_count = 0
+        # for row in self.screen_configs.get("world_data"): 
+        for row in self.screen_configs.get("levels").get(str(current_level)): 
+            col_count = 0
+            for tile in row: 
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (self.screen_configs.get("tile_size"), self.screen_configs.get("tile_size")))
+                    img_rect = img.get_rect() 
+                    img_rect.x = col_count * img_rect.width
+                    img_rect.y = row_count * img_rect.height
+                    tile = (img, img_rect) 
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (self.screen_configs.get("tile_size"), self.screen_configs.get("tile_size")))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * img_rect.width
+                    img_rect.y = row_count * img_rect.height
+                    tile = (img, img_rect) 
+                    self.tile_list.append(tile)
+                
+                
+                if tile == "blob":
+                    enemy_path_image = enemy_configs.get("blob").get("path_image")
+                    enemy_height_image = enemy_configs.get("blob").get("height_image")
+                    compensation_y = self.screen_configs.get("tile_size") - enemy_height_image
+                    # enemy = Enemy(enemy_path_image, 
+                    #              col_count * self.screen_configs.get("tile_size"), 
+                    #              row_count * self.screen_configs.get("tile_size") + compensation_y)
+                    
+                    enemy = Blob(enemy_path_image, 
+                                 col_count * self.screen_configs.get("tile_size"), 
+                                 row_count * self.screen_configs.get("tile_size") + compensation_y)
+
+                    enemy_sprite_group.add(enemy)
+
+                if tile == 4: # plataformas con movimiento en eje X v12
+
+                    coord_x = col_count * self.screen_configs.get("tile_size")
+                    coord_y = row_count * self.screen_configs.get("tile_size")
+                    tile_size = self.screen_configs.get("tile_size")
+
+                    platform = Platform(coord_x, coord_y, tile_size, 1, 0)
+                    platform_group.add(platform)
+
+                if tile == 5: # plataformas con movimiento en eje Y v12
+
+                    coord_x = col_count * self.screen_configs.get("tile_size")
+                    coord_y = row_count * self.screen_configs.get("tile_size")
+                    tile_size = self.screen_configs.get("tile_size")
+
+                    platform = Platform(coord_x, coord_y, tile_size, 0, 1)
+                    platform_group.add(platform)
+
+
+                if tile == "lava":
+
+                    enemy_path_image = enemy_configs.get("lava").get("path_image")
+                    enemy_height_image = enemy_configs.get("lava").get("height_image")
+                    coord_x = col_count * self.screen_configs.get("tile_size")
+                    coord_y = row_count * self.screen_configs.get("tile_size") + (self.screen_configs.get("tile_size") // 2)
+                    lava = Lava(enemy_path_image, 
+                                coord_x, 
+                                coord_y,
+                                self.screen_configs.get("tile_size")
+                                )
+                    enemy_sprite_group.add(lava)
+                
+                if tile == 7:
+                    coord_x = col_count * self.screen_configs.get("tile_size") + (self.screen_configs.get("tile_size")//2)
+                    coord_y = row_count * self.screen_configs.get("tile_size") +  (self.screen_configs.get("tile_size")//2)
+                    coin = Coin(coord_x, coord_y, self.screen_configs.get("tile_size"))
+                    coin_group.add(coin)
+
+
+                if tile == 8:
+                    exit_path_image = utilities_configs.get("exit").get("path_image")
+                    coord_x = col_count * self.screen_configs.get("tile_size")
+                    coord_y = row_count * self.screen_configs.get("tile_size") - (self.screen_configs.get("tile_size") // 2)
+                    exit = Exit(exit_path_image, coord_x, coord_y, self.screen_configs.get("tile_size"))
+                    utilities_group.add(exit)
+                    # exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    # exit_group.add(exit)
+                    
+                col_count += 1
+            row_count += 1
+
+    def draw_backgroung(self, screen):
+        screen.blit(pygame.image.load(self.screen_configs.get("images").get("sky")) , (0,0))
+        screen.blit(pygame.image.load(self.screen_configs.get("images").get("sun")) , (100,100))
+        
+    def draw_grid(self, screen):
+        for line in range(0, int(self.screen_configs.get("screen_width")/self.screen_configs.get("tile_size"))):
+            pygame.draw.line(surface=screen, color=(255, 255, 255), start_pos=(0, line * self.screen_configs.get("tile_size")), end_pos=(self.screen_configs.get("screen_width"), line * self.screen_configs.get("tile_size")), width=1)
+            pygame.draw.line(surface=screen, color=(255, 255, 255), start_pos=(line * self.screen_configs.get("tile_size"), 0), end_pos=(line * self.screen_configs.get("tile_size"), self.screen_configs.get("screen_height")), width=1)
+          
+    def draw(self, screen):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+    # function to reset level
+    # @staticmethod
+    def reset_level(
+            self, 
+            screen_configs, 
+            enemy_configs, 
+            enemy_sprite_group, 
+            utilities_configs, 
+            utilities_group, 
+            coin_group, 
+            platform_group, 
+            current_level):
+        
+        world = World(
+                    screen_configs, 
+                    enemy_configs, 
+                    enemy_sprite_group, 
+                    utilities_configs, 
+                    utilities_group, 
+                    coin_group, 
+                    platform_group, 
+                    current_level) # nueva instancia de World con el mapa del nivel que corresponda
+        
+        return world
+    
+
+
+        # player.reset(100, screen_height - 50) # reseteo los atributos del platyer para que se blitee en la posicion inicial luego de pasar de nivel y ya en el nuevo escenario
+        # # player.reset(850, 50)
+        # blob_group.empty() # empty() -> metodo de la clase Group() que elimina todos los sprites de un objeto Group
+        # lava_group.empty()
+        # exit_group.empty()
+        # if path.exists(f"level{level}_data"): # load in level data and create world
+        #     pickle_in = open(f"level{level}_data", "rb") # read binary
+        #     world_data = pickle.load(pickle_in)
+        #     pickle_in.close()
+        # world = World(world_data) # nueva instancia de World con el mapa del nivel que corresponda
+        # return world
+
+    
+    def draw_text(self, screen, text_info: tuple):
+
+        tipo_texto_a_imprimir = text_info[0] # score|game_over|you_win
+
+        informacion_texto = self.screen_configs.get("texts").get(text_info[0])
+
+
+        if tipo_texto_a_imprimir == "score":
+            score = text_info[1] 
+            texto_a_imprimir = informacion_texto.get('text').format(str(score))
+        else:
+            texto_a_imprimir = informacion_texto.get('text')
+
+
+        tipo_fuente = informacion_texto.get('font')
+        tamanio_fuente = informacion_texto.get('size')
+        color = eval(informacion_texto.get('color'))
+        x = informacion_texto.get('coord_x')
+        y = informacion_texto.get('coord_y')
+
+        # print(informacion_texto)
+        
+
+
+        instancia_Font = Font(tipo_fuente, tamanio_fuente)
+        superficie = instancia_Font.surface_text
+        # print(font_score_surface)
+
+
+
+        # antialias = True
+        img = superficie.render(texto_a_imprimir, True, color) 
+        
+        screen.blit(img, (x,y))
+
+    @staticmethod
+    def inicializar_sonidos():
+        pygame.mixer.music.load("img/music.wav")
+        pygame.mixer.music.play(-1, 0.0, 5000) 
+        # param 5000 -> volumen musica de fondo in crescendo durante 5 segs hasta llegar al 100% del volumen seteado
+        pygame.mixer.music.set_volume(0.025)
+    
+        coin_fx = pygame.mixer.Sound("img/coin.wav")
+        coin_fx.set_volume(0.05)
+        jump_fx = pygame.mixer.Sound("img/jump.wav")
+        jump_fx.set_volume(0.05)
+        game_over_fx = pygame.mixer.Sound("img/game_over.wav")
+        game_over_fx.set_volume(0.05)
+        return (coin_fx, jump_fx, game_over_fx)
