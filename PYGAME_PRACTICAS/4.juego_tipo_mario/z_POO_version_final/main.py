ec4c6fbbfc266fa969585c147e7b61e0203ad2de
lionel-prats <lionel.prats.c@gmail.com>
2023-11-26 18:58:45 -0300

    domingo 26/11 - commit 15

--- PYGAME_PRACTICAS/4.juego_tipo_mario/z_POO_version_final/main.py
+++ PYGAME_PRACTICAS/4.juego_tipo_mario/z_POO_version_final/main.py
@@ -21,10 +21,11 @@ screen_dimentions = (configs.get("screen").get("screen_width"),
 screen = pygame.display.set_mode(screen_dimentions)
 pygame.display.set_caption("POO - Version Final")
 
-current_level, initial_score, main_menu, max_levels, player_dead, player_win, playing, step_add_level = configs.get("game_variables").values()
+initial_level, initial_score, main_menu, max_levels, player_dead, player_win, playing, step_add_level = configs.get("game_variables").values()
 
 # define game variables
 main_menu = True
+current_level = initial_level
 score = initial_score
 game_status = playing
 
@@ -34,7 +35,7 @@ background_surface = pygame.image.load(background_image_path)
 sun_img_path, sun_img_coord_x, sun_img_coord_y = configs.get("screen").get("images").get("sun").values()
 sun_surface = pygame.image.load(sun_img_path)
 
-player = Player(configs.get("player1"))
+player = Player(configs.get("player"))
 
 enemies_group = pygame.sprite.Group()
 platform_group = pygame.sprite.Group()
@@ -75,13 +76,13 @@ while run:
 
         world.draw(screen)
 
-        # group.draw() -> metodo de la clase Group para blitear sprites de un objeto-instancia
+        # group.draw() -> metodo de la clase Group para blitear sprites de un grupo instancia de la clase
         platform_group.draw(screen)
         enemies_group.draw(screen)
         coin_group.draw(screen)
         exit_group.draw(screen)
 
-        if game_status == playing: # playing
+        if game_status == playing:
 
             platform_group.update()
             enemies_group.update()
@@ -104,13 +105,13 @@ while run:
         # update score
         world.draw_text(screen, ("score", score, coin_group))
 
-        if game_status == player_dead: # player has died
+        if game_status == player_dead: 
 
             world.draw_text(screen, ("game_over",))
             
             if restart_button.draw(screen): # restart button is pressed
                 
-                player.reset(configs.get("player1"))
+                player.reset(configs.get("player"))
                 game_status = playing
                 score = 0 
                 
@@ -123,7 +124,7 @@ while run:
                                           exit_group=exit_group, coin_group=coin_group,
                                           platform_group=platform_group, current_level=current_level)
 
-        if game_status == player_win: # player has completed the level 
+        if game_status == player_win:
 
             current_level += step_add_level
 
@@ -133,7 +134,7 @@ while run:
                 exit_group.empty()
                 platform_group.empty()
 
-                player.reset(configs.get("player1"))
+                player.reset(configs.get("player"))
 
                 world = world.reset_level(configs.get("screen"), configs.get("enemies"), 
                                           enemy_sprite_group=enemies_group, exit_configs=configs.get("exit"),
@@ -146,20 +147,22 @@ while run:
 
                 world.draw_text(screen, ("you_win",))
 
-                if restart_button.draw(screen): 
+                if restart_button.draw(screen): # restart game (first level)
                     
-                    current_level = 1 # restart game (level 1)
+                    current_level = initial_level
 
                     enemies_group.empty()
                     exit_group.empty()
                     platform_group.empty()
 
-                    player.reset(configs.get("player1"))
+                    player.reset(configs.get("player"))
 
                     world = world.reset_level(configs.get("screen"), configs.get("enemies"), 
-                                              enemy_sprite_group=enemies_group, exit_configs=configs.get("exit"),
+                                              enemy_sprite_group=enemies_group, 
+                                              exit_configs=configs.get("exit"),
                                               exit_group=exit_group, coin_group=coin_group,
-                                              platform_group=platform_group, current_level=current_level)
+                                              platform_group=platform_group, 
+                                              current_level=current_level)
                     
                     game_status = playing # habilito que se siga moviendo el player y los enemigos
                     score = 0 
